f07062351b66abf6378ce7b5244ebbf066a53e20
Farrell Adebo <168660530+fadebo@users.noreply.github.com>
2024-07-21 23:48:32 +0000

    The footer, header, public header, and their styles have been added.
    Just a quick heads-up, we've got the footer, header, public header, and their styles all set up and good to go!

---
+++ app/views/templates/header.php
@@ -0,0 +1,51 @@
+<?php
+if (!isset($_SESSION['auth'])) {
+    header('Location: /login');
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="icon" href="/favicon.png">
+        <title>COSC 4806</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
+        <link rel="stylesheet" href="/app/public/css/styles.css">
+      <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    </head>
+    <body>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">COSC 4806</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="/home">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/movie">Movie Search</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Movie List</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/secret">Secret</a>
+                </li>
+              </ul>
+              <span class="navbar-text">
+                Welcome, <?php echo $_SESSION['username']; ?>!
+              </span>
+            </div>
+          </div>
+        </nav>
---
+++ app/views/templates/headerPublic.php
@@ -0,0 +1,19 @@
+<?php
+if (isset($_SESSION['auth']) == 1) {
+    header('Location: /home');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" href="/favicon.png">
+    <title>COSC 4806</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+    <link rel="stylesheet" href="/app/public/css/publicstyles.css">
+</head>
+<body>
