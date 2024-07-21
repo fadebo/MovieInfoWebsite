bf475f62462fb842f323644c9ec14d735e31614e
Farrell Adebo <168660530+fadebo@users.noreply.github.com>
2024-07-21 23:55:58 +0000

    Implemented the core functionality and established the connection to the database.

---
+++ app/core/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+class Controller {
+
+    public function model ($model) {
+        require_once 'app/models/' .$model . '.php';
+        return new $model();
+    }
+
+    public function view ($view, $data = []) {
+        // Extract the data array into individual variables
+        extract($data);
+        // Include the view file
+        require_once 'app/views/' . $view .'.php';
+    }
+
+}
